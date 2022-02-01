d1abafd4d5cba7297cecec7b893489ebb5c94f47
Maxim A.Jurankov <astronmax@yandex.ru>
2022-02-01 14:05:57 +0300

    Add basic functional of login page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import re
 
 auth = Blueprint('auth', __name__)
 
+
+@auth.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email, password)
+
+    return render_template('login.html')
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        gender = request.form.get('gender')
+        birthday = request.form.get('birthday')
+        print(username, email, gender, password, birthday)
+
+    return render_template('sign_up.html')
